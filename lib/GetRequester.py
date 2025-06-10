9086b23a5657c879fac1e5ddec6216efa241437e
luciayin9944 <luxiayin94@gmail.com>
2025-06-10 11:08:41 -0700

    implement class GetRequester

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,23 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content ##return bytes: b'[{"name": "Daniel", ...}]'
+        #return response.json() ##return parsed json: [{"name": "Daniel", ...}]
+        #return response.text ##return str: '[{"name": "Daniel", ...}]'
+        
+
 
     def load_json(self):
-        pass
+        data = self.get_response_body()
+        return json.loads(data) ##parsed json: [{"name": "Daniel", ...}]
+
+
+
+# url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+# requester = GetRequester(url)
+
+
+# data = requester.get_response_body()
+# for person in data:
+#     print(f"{person['name']} is a {person['occupation']}")
