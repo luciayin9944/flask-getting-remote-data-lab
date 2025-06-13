904ad13cd99a6357cdfb34e65f6c589ff958ee01
luciayin9944 <luxiayin94@gmail.com>
2025-06-13 16:32:26 -0700

    delete comments

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -9,21 +9,13 @@ class GetRequester:
     def get_response_body(self):
         response = requests.get(self.url)
         return response.content ##return bytes: b'[{"name": "Daniel", ...}]'
+    
+    """
         #return response.json() ##return parsed json: [{"name": "Daniel", ...}]
         #return response.text ##return str: '[{"name": "Daniel", ...}]'
-        
-
+    """
 
     def load_json(self):
         data = self.get_response_body()
         return json.loads(data) ##parsed json: [{"name": "Daniel", ...}]
 
-
-
-# url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-# requester = GetRequester(url)
-
-
-# data = requester.get_response_body()
-# for person in data:
-#     print(f"{person['name']} is a {person['occupation']}")
